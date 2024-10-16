e4fba938494f9bd253621bdd9622af0cf9076e80
Dillon <162556809+dillguill@users.noreply.github.com>
2024-10-16 18:08:33 -0500

    Fixed search issue

--- src/components/SearchView.js
+++ src/components/SearchView.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const MovieCard = ({ movie }) => {
     const posterUrl = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-    const detailUrl = `movies/${movie.id}`
+    const detailUrl = `/movies/${movie.id}`
     return (
         <div className="col-lg-3 col-md-3 col-2 my-4">
             <div className="card">
