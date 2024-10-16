4aba7ce42da4a907563433922eaccdcdd12cd4b0
Dillon <162556809+dillguill@users.noreply.github.com>
2024-10-16 17:38:00 -0500

    Movie detail component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
-import { useState, useEffect } from "react"; 
+import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import HomeView from "./components/HomeView";
 import AboutView from "./components/AboutView";
+import MovieView from "./components/MovieView";
 import SearchView from "./components/SearchView";
 import { Routes, Route } from "react-router-dom";
 
@@ -12,15 +13,17 @@ function App() {
 
   useEffect(() => {
     // console.log(searchText, "is the search text")
-   if(searchText) {
-     fetch(`https://api.themoviedb.org/3/search/movie?api_key=bfe72e251205c6f02b8dcd2ba5e3a582&language=en-US&query=${searchText}&page=1&include_adult=false`)
-       .then(response => response.json())
-       .then(data => {
-         // console.log(data)
-         setSearchResults(data.results)
-       })
-   }
-  }, [searchText])
+    if (searchText) {
+      fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=bfe72e251205c6f02b8dcd2ba5e3a582&query=${searchText}&language=en-US&include_adult=false`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          // console.log(data)
+          setSearchResults(data.results);
+        });
+    }
+  }, [searchText]);
 
   return (
     <div>
@@ -28,6 +31,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomeView />} />
         <Route path="/about" element={<AboutView />} />
+        <Route path="/movies/:id" element={<MovieView />} />
         <Route
           path="/search"
           element={
--- src/components/SearchView.js
+++ src/components/SearchView.js
@@ -1,16 +1,16 @@
 import Hero from "./Hero";
-
-// TMDB API Key: bfe72e251205c6f02b8dcd2ba5e3a582
+import { Link } from "react-router-dom";
 
 const MovieCard = ({ movie }) => {
-    const posterURL = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+    const posterUrl = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+    const detailUrl = `movies/${movie.id}`
     return (
         <div className="col-lg-3 col-md-3 col-2 my-4">
             <div className="card">
-                <img src={posterURL} className="card-img-top" alt={movie.original_title} />
+                <img src={posterUrl} className="card-img-top" alt={movie.original_title} />
                 <div className="card-body">
                     <h5 className="card-title">{movie.original_title}</h5>
-                    <a href="#" className="btn btn-primary">Show Details</a>
+                    <Link to={detailUrl} className="btn btn-primary">Show Details</Link>
                 </div>
             </div>
         </div>
